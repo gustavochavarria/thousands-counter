a0c4783298c69bbe0b898fa0a88b16073377e664
Fernando Larios <fernando@penguinprotocols.com>
2019-10-04 16:27:23 -0600

    changing name variable

--- index.js
+++ index.js
@@ -5,19 +5,19 @@ const oneBillion = Math.pow(1000,3); // billion US = thousands of millions
 export const toAbr = (number, digits) => {
     let abr = number;
     let symbol = '';
-    const abbrev = ["K","M","B"];
+    const symbolList = ["K","M","B"];
 
     if(isNaN(number) || number < 0) {
         return 0;
     }
 
-    for (var i = abbrev.length - 1; i >= 0; i--) {
+    for (var i = symbolList.length - 1; i >= 0; i--) {
         // References of thounsands
         var reference = Math.pow(1000,parseInt(i + 1));
 
         if (number >= reference) {
             abr = number/reference;
-            symbol = abbrev[i];
+            symbol = symbolList[i];
             break;
         }
     }
