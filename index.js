94759de1097f4cc3ddb536bf02361573b6f95704
Hosmel Quintana <hosmelq@gmail.com>
2019-10-08 12:42:37 -0600

    add some dotfiles and configure prettier

--- index.js
+++ index.js
@@ -1,29 +1,25 @@
-const oneThousand = 1000;
-const oneMillion = Math.pow(1000,2);
-const oneBillion = Math.pow(1000,3); // billion US = thousands of millions
+const oneThousand = 1000
+const oneMillion = 1000 ** 2
+const oneBillion = 1000 ** 3 // Billion US = thousands of millions
 
 export const toAbr = (number, digits) => {
-    let abr = number;
-    let symbol = '';
-    const symbolList = ["K","M","B"];
+  let abr = number
+  let symbol = ''
+  const symbolList = ['K', 'M', 'B']
 
-    if(isNaN(number) || number < 0) {
-        return 0;
+  if (isNaN(number) || number < 0) {
+    return 0
+  }
+
+  for (let i = symbolList.length - 1; i >= 0; i--) {
+    const reference = 1000 ** parseInt(i + 1)
+
+    if (number >= reference) {
+      abr = number / reference
+      symbol = symbolList[i]
+      break
     }
+  }
 
-    for (var i = symbolList.length - 1; i >= 0; i--) {
-        // References of thounsands
-        var reference = Math.pow(1000,parseInt(i + 1));
-
-        if (number >= reference) {
-            abr = number/reference;
-            symbol = symbolList[i];
-            break;
-        }
-    }
-
-    return number < 1000 ? number : `${parseFloat(abr).toFixed(digits)}${symbol}`;
+  return number < 1000 ? number : `${parseFloat(abr).toFixed(digits)}${symbol}`
 }
-
-//Acept negative 
-//export const toAbrWithSignal = () => {}
