fdec1c5db9f4a1a90ffeac34a5fdd3f11ecefe1c
tavo <gchava92@gmail.com>
2019-10-09 10:37:52 -0600

    Added tests

---
+++ __tests__/index.js
@@ -0,0 +1,21 @@
+import test from 'ava';
+import tc from '..';
+
+test('shoul be 700', t => {
+  t.is(tc(700, 1), '700.0');
+});
+
+test('should be 1K', t => {
+  const oneK = tc(1000, 1);
+  t.is(oneK, '1.0K');
+});
+
+test('should be 1.5M', t => {
+  const oneMillion = tc(1500000, 1);
+  t.is(oneMillion, '1.5M');
+});
+
+test('should be a -1.24K', t => {
+  const oneK = tc(-1240, 2);
+  t.is(oneK, '-1.24K');
+});
